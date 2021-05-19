5269056b1e28665b828cba0ab0892bc5add2ceca
Nikolaj Grigorjev <grigorjev.nikolaj@gmail.com>
2021-05-19 10:39:24 +0300

    Hello world and security removed

--- src/main/java/com/nikgri/demospringpizzaapi/DemoSpringPizzaApiApplication.java
+++ src/main/java/com/nikgri/demospringpizzaapi/DemoSpringPizzaApiApplication.java
@@ -2,12 +2,20 @@ package com.nikgri.demospringpizzaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoSpringPizzaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringPizzaApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(){
+		return "Hello world!";
 	}
 
 }
